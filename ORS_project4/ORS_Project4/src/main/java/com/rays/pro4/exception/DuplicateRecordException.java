66b6ff25230d11c04c74585810a7fc09105dba75
teenaprajapat123 <teenaprajapat435@gmail.com>
2022-12-09 14:59:25 +0530

    new

---
+++ ORS_project4/ORS_Project4/src/main/java/com/rays/pro4/exception/DuplicateRecordException.java
@@ -0,0 +1,17 @@
+package com.rays.pro4.exception;
+
+/**
+ * DuplicateRecordException thrown when a duplicate record occurred.
+ * 
+ * @author TEENA PRAJAPAT
+ *
+ */
+
+public class DuplicateRecordException extends Exception {
+
+	public DuplicateRecordException(String msg){
+		super(msg);
+	}
+
+	
+}
